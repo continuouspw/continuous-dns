703b1e96722a1c07f9e9b0742464d69a6776a2ed
Logan V <logan2211@gmail.com>
2018-06-10 11:28:32 -0500

    Add ipv6 dynhost entries

--- playbooks/files/dynhost6.py
+++ playbooks/files/dynhost6.py
@@ -19,16 +19,29 @@ from sys import stdin, stdout
 from time import time
 
 data = stdin.readline()
-stdout.write("OK\tDynhost v4 Backend\n")
+stdout.write("OK\tDynhost v6 Backend\n")
 stdout.flush()
 
 
-def hex2ip(hexip):
+def splitCount(s, count):
+    return [''.join(x) for x in zip(*[list(s[z::count]) for z in range(count)])]
+
+def ipv6ize(address):
+    # Convert a string like fd64baa481f3b83a into
+    # fd64:baa4:81f3:b83a:0000:0000:0000:0000
+
+    # Zerofill to 32 characters to ensure full v6 address
+    # Then split the string by 4 char chunks and join with ':'
+    return ':'.join(splitCount('{:0<32s}'.format(address), 4))
+
+def hex2ip6(hexip):
     # Return an IP address from a hex encoded decimal IP
     try:
-        ip = socket.inet_ntoa(struct.pack('!L', int(hexip, 16)))
-    except (ValueError, struct.error):
-        ip = '127.0.0.1'
+        ip = socket.inet_ntop(socket.AF_INET6,
+                              socket.inet_pton(socket.AF_INET6,
+                                               ipv6ize(hexip)))
+    except (ValueError, socket.error):
+        ip = '::1'
     return ip
 
 
@@ -47,9 +60,9 @@ while True:
           'issue "letsencrypt.org"'
         )
     else:
-        resp_ip = hex2ip(qname.split('.')[0])
+        resp_ip = hex2ip6(qname.split('.')[0])
         r = "DATA\t{}\t{}\t{}\t{}\t{}\t{}\n".format(
-          qname, qclass, 'A', 86400, id, resp_ip
+          qname, qclass, 'AAAA', 86400, id, resp_ip
         )
     stdout.write(r)
     stdout.write("END\n")
